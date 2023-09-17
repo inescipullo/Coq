a0b03480c1d0cd32c0c0465bd4faea7ae427ecfa
Katherine Sullivan <64032284+katherinesullivan@users.noreply.github.com>
2023-09-17 15:25:11 -0300

    Ejs 12 13 14 15

--- Practica3/plantilla3K.v
+++ Practica3/plantilla3K.v
@@ -390,7 +390,24 @@ End Ejercicio11.
 
 
 Section Ejercicio12.
-...
+
+Parameter  AVLNat : forall n : nat, Set.
+Parameter emptyAVL : AVLNat 0.
+Parameter addAVL1 : forall n:nat, nat -> AVLNat n -> AVLNat (plus n 1) -> AVLNat (S (S n)).
+Parameter addAVL2 : forall n:nat, nat -> AVLNat n -> AVLNat n -> AVLNat (S n).
+Parameter addAVL3 : forall n:nat, nat -> AVLNat n -> AVLNat (minus n 1) -> AVLNat (S n).
+
+Definition LeftLeaf := addAVL2 0 3 emptyAVL emptyAVL.
+
+Definition AVL2 := addAVL3 1 10 LeftLeaf emptyAVL.
+Check AVL2.
+
+Parameter GenAVL : forall n : nat, Set.
+Parameter GenEmptyAVL : AVLNat 0.
+Parameter GenAddAVL1 : forall (n:nat) (X:Set), X -> AVLNat n -> AVLNat (plus n 1) -> AVLNat (S (S n)).
+Parameter GenAddAVL2 : forall (n:nat) (X:Set), X -> AVLNat n -> AVLNat n -> AVLNat (S n).
+Parameter GenAddAVL3 : forall (n:nat) (X:Set), X -> AVLNat n -> AVLNat (minus n 1) -> AVLNat (S n).
+
 End Ejercicio12.
 
 
@@ -399,17 +416,17 @@ Variable A B C: Set.
 
 Lemma e13_1 : (A -> B -> C) -> B -> A -> C.
 Proof.
-  ...
+  exact (fun (H1 : A -> B -> C) (H2 : B) (H3 : A) => H1 H3 H2).
 Qed.
 
 Lemma e13_2 : (A -> B) -> (B -> C) -> A -> C.
 Proof.
-  ...
+  exact (fun (H1 : A -> B) (H2 : B -> C) (H3 : A) => H2 (H1 H3)).
 Qed.
 
 Lemma e13_3 : (A -> B -> C) -> (B -> A) -> B -> C.
 Proof.
-  ...
+  exact (fun (H1 : A -> B -> C) (H2 : B -> A) (H3 : B) => H1 (H2 H3) H3).
 Qed.
 
 End Ejercicio13.
@@ -421,19 +438,19 @@ Variable A B C: Prop.
 Lemma Ej314_1 : (A -> B -> C) -> A -> (A -> C) -> B -> C.
 Proof.
   intros f a g b.
-        ...
+  exact (g a).
 Qed.
 
 Lemma Ej314_2 : A -> ~ ~ A.
 Proof.
   unfold not.
   intros.
-     ...
+  exact (H0 H).
 Qed.
 
 Lemma Ej314_3 : (A -> B -> C) -> A -> B -> C.
 Proof.
-     ...
+  exact (fun (H1 : A -> B -> C) (H2 : A) (H3 : B) => H1 H2 H3).
 Qed.
 
 Lemma Ej314_4 : (A -> B) -> ~ (A /\ ~ B).
@@ -441,7 +458,7 @@ Proof.
   unfold not.
   intros.
   elim H0; intros.
-     ...
+  exact (H2 (H H1)).
 Qed.
 
 End Ejercicio14.
@@ -460,30 +477,30 @@ Lemma Ej315_1 : (forall x : U, A x -> B x) -> (forall x : U, A x) ->
 forall x : U, B x.
 Proof.
   intros.
-   ...
+  exact (H x (H0 x)).
 Qed.
 
 Lemma Ej315_2 : forall x : U, A x -> ~ (forall x : U, ~ A x).
 Proof.
   unfold not.
   intros.
-  ...
+  exact (H0 x H).
 Qed.
 
 Lemma Ej315_3 : (forall x : U, P -> A x) -> P -> forall x : U, A x.
 Proof.
-    ...
+  exact (fun (H1 : forall x : U, P -> A x) (H2 : P) (H3 : U) => H1 H3 H2).
 Qed.
 
 Lemma Ej315_4 : (forall x y : U, R x y) -> forall x : U, R x x.
 Proof.
-     ...
+  exact (fun (H1 : forall x y : U, R x y) (H2 : U) => H1 H2 H2).
 Qed.
 
 Lemma Ej315_5 : (forall x y: U, R x y -> R y x) ->
                  forall z : U, R e z -> R z e.
 Proof.
-     ...
+  exact (fun (H1 : forall x y: U, R x y -> R y x) (z : U) (H2 : R e z) => H1 e z H2).
 Qed.
 
 End Ejercicio15.
