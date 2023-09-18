83cc94d257cdb4c0b56b29fe9552bb50d2082b85
Ines <inescipullo@gmail.com>
2023-09-17 21:15:57 -0300

    Agregue la entrega de la Practica 3

---
+++ Practica3/P3-Cipullo-Sullivan.v
@@ -0,0 +1,248 @@
+(* Entrega Práctico 3 - CFPTT *)
+(* Alumnas: Cipullo, Inés - Sullivan, Katherine *)
+
+
+(* Definición necesaria para el desarrollo del ejercicio 4 *)
+Definition oGen: forall A B C: Set, (A->B)->(B->C)->(A->C) := fun a b c (f: a->b) (g: b->c) => fun x: a => g (f x). 
+Check oGen.
+
+
+(* Ejercicio 4 *)
+Section Ejercicio4.
+Variable A: Set.
+
+Definition id: A-> A := fun x:A => x.
+
+Theorem e4_1 : forall x:A, (oGen A A A id id) x = id x.
+Proof.
+intro x.
+compute.
+reflexivity.
+Qed.
+
+Theorem e4_2 : forall x:A, (oGen A A A id id) x = id x.
+Proof.
+intro x.
+cbv delta beta.
+reflexivity.
+Qed.
+
+Theorem e4_3 : forall x:A, (oGen A A A id id) x = id x.
+Proof.
+intro x.
+cbv delta.
+simpl. (* funciona porque oGen es constante transparente *)
+reflexivity.
+Qed.
+
+End Ejercicio4.
+
+
+(* Ejercicio 5 *)
+Section Ejercicio5.
+
+(* 5.1 *)
+Definition opI (A : Set) (x : A) := x.
+
+Definition opK (A : Set) (B : Set) (x : A) (y : B) := x.
+
+Definition opS (A : Set) (B: Set) (C : Set) (f : A -> B -> C) (g : A -> B) (x : A) := (f x) (g x).
+
+Lemma e5je : forall A B : Set, opS A (B->A) A (opK A (B->A)) (opK A B)  = opI A.
+Proof.
+cbv delta.
+cbv beta.
+reflexivity.
+Qed.
+
+(* 5.2 *)
+(* Para formalizar el siguiente lema, determine los tipos ?1 ... ?8 adecuados*) 
+Lemma e52 : forall A B : Set, opS A (B->A) A (opK A (B->A)) (opK A B) = opI A.
+Proof.
+cbv delta beta.
+reflexivity.
+Qed.
+
+End Ejercicio5.
+
+
+(* Ejercicio8 *)
+Section ArrayNat.
+
+Parameter ArrayNat : forall n:nat, Set.
+Parameter empty    : ArrayNat 0.
+Parameter add      : forall n:nat, nat -> ArrayNat n -> ArrayNat (n + 1).
+
+(* 8.1 *)
+Check ((add 0 (S 0) empty) : ArrayNat (S 0)).
+
+(* 8.2 *)
+(* Vector [0,0] *)
+Check (add 1 0 (add 0 0 empty): ArrayNat 2).
+
+(* Vector [0,1,0,1 *)
+Check (add 3 0 (add 2 1 (add 1 0 (add 0 1 empty))): ArrayNat 4).
+
+(* 8.3 *)
+Parameter Concat : forall n1: nat, forall n2 :nat, ArrayNat n1 -> ArrayNat n2 -> ArrayNat (plus n1 n2). 
+
+(* 8.4 *)
+Parameter Zip : forall n: nat, ArrayNat n -> ArrayNat n -> (nat->nat->nat) -> ArrayNat n.
+
+(* 8.5 *)
+Check (ArrayNat: nat -> Set).
+
+(* 8.6 *)
+Parameter ArrayGen : Set -> nat -> Set.
+Parameter emptyGen : forall X: Set, ArrayGen X 0.
+Parameter addGen   : forall X: Set, forall n: nat, X -> ArrayGen X n -> ArrayGen X (n+1).
+Parameter ZipGen   : forall X: Set, forall n: nat, ArrayGen X n -> ArrayGen X n -> (nat->nat->nat) -> ArrayGen X n.
+
+(* 8.7 *)
+Parameter ArrayBool : forall n: nat, ArrayGen bool n.
+
+End ArrayNat.
+
+
+(* Ejercicio 10 *)
+Section Ejercicio10.
+
+Parameter Array : Set -> nat -> Set.
+Parameter emptyA : forall X : Set, Array X 0.
+Parameter addA : forall (X : Set) (n : nat), X -> Array X n -> Array X (S n).
+
+Parameter Matrix : Set -> nat -> Set.
+Parameter emptyM : forall (X : Set) (m:nat), Matrix (Array X m) 0. (* permite crear todas las matrices de 0 columnas *)
+Parameter addM : forall (X : Set) (m n : nat), Array X m -> Matrix (Array X m) n -> Matrix (Array X m) (S n). (* crea matrices m X n *)
+
+(* Tomamos como cantidad de filas 3. Si la cantidad de filas que tiene una matriz escalonada es 
+mayor a la cantidad de columnas, existen filas nulas, lo que no aporta informacion *)
+Definition A1 := addA nat 2 1 (addA nat 1 0 (addA nat 0 0 (emptyA nat))). (*columna de longitud 3 con un 1 al inicio*)
+Definition A2 := addA nat 2 2 (addA nat 1 2 (addA nat 0 0 (emptyA nat))). (*columna de longitud 3 con dos 2 al inicio*)
+Definition A3 := addA nat 2 3 (addA nat 1 3 (addA nat 0 3 (emptyA nat))). (*columna de longitud 3 con tres 3 al inicio*)
+
+Definition M1 := addM nat 3 0 A1 (emptyM nat 3).  (* matriz de una columna *)
+Definition M2 := addM nat 3 1 A2 M1. (* matriz de dos columnas *) 
+Definition M3 := addM nat 3 2 A3 M2. (* matriz de tres columnas *)
+
+Check M3.
+
+End Ejercicio10.
+
+
+(* Ejercicio 11 *)
+Section Ejercicio11.
+
+Parameter ABNat : forall n : nat, Set.
+
+Parameter emptyAB : ABNat 0.
+
+Parameter addAB : forall n : nat, ArrayNat (Nat.pow 2 n) -> ABNat n -> ABNat (S n). (* A un árbol de altura n le paso como array un nuevo nivel *)
+
+Definition AB2 := addAB 1 (add 1 7 (add 0 7 empty)) (addAB 0 (add 0 3 empty) (emptyAB)).
+Check AB2.
+
+Parameter GenAB : Set -> nat -> Set.
+Parameter GenEmptyAB : forall X : Set, GenAB X 0.
+Parameter GenAddAB : forall (X:Set) (n:nat), Array X (Nat.pow 2 n) -> GenAB X n -> GenAB X (S n).
+
+End Ejercicio11.
+
+
+(* Ejercicio 12 *)
+Section Ejercicio12.
+
+Parameter  AVLNat : forall n : nat, Set.
+Parameter emptyAVL : AVLNat 0.
+(* No pienso en agregar un elemento al árbol, sino en contruir un árbol. Le paso la raiz,
+el árbol izq y el árbol der *)
+Parameter addAVL1 : forall n:nat, nat -> AVLNat n -> AVLNat (plus n 1) -> AVLNat (S (S n)).
+Parameter addAVL2 : forall n:nat, nat -> AVLNat n -> AVLNat n -> AVLNat (S n).
+Parameter addAVL3 : forall n:nat, nat -> AVLNat n -> AVLNat (minus n 1) -> AVLNat (S n).
+
+Definition LeftLeaf := addAVL2 0 3 emptyAVL emptyAVL.
+
+Definition AVL2 := addAVL3 1 10 LeftLeaf emptyAVL.
+Check AVL2.
+
+Parameter GenAVL : forall n : nat, Set.
+Parameter GenEmptyAVL : AVLNat 0.
+Parameter GenAddAVL1 : forall (n:nat) (X:Set), X -> AVLNat n -> AVLNat (plus n 1) -> AVLNat (S (S n)).
+Parameter GenAddAVL2 : forall (n:nat) (X:Set), X -> AVLNat n -> AVLNat n -> AVLNat (S n).
+Parameter GenAddAVL3 : forall (n:nat) (X:Set), X -> AVLNat n -> AVLNat (minus n 1) -> AVLNat (S n).
+
+End Ejercicio12.
+
+
+(* Ejercicio 14 *)
+Section Ejercicio14.
+Variable A B C: Prop.
+
+Lemma Ej314_1 : (A -> B -> C) -> A -> (A -> C) -> B -> C.
+Proof.
+  intros f a g b.
+  exact (g a).
+Qed.
+
+Lemma Ej314_2 : A -> ~ ~ A.
+Proof.
+  unfold not.
+  intros.
+  exact (H0 H).
+Qed.
+
+Lemma Ej314_3 : (A -> B -> C) -> A -> B -> C.
+Proof.
+  exact (fun (H : A -> B -> C) => H).
+Qed.
+
+Lemma Ej314_4 : (A -> B) -> ~ (A /\ ~ B).
+Proof.
+  unfold not.
+  intros.
+  elim H0; intros.
+  exact (H2 (H H1)).
+Qed.
+
+End Ejercicio14.
+
+
+(* Ejercicio 15 *)
+Section Ejercicio15.
+
+Variable U : Set.
+Variable e : U.
+Variable A B : U -> Prop.
+Variable P : Prop.
+Variable R : U -> U -> Prop.
+
+Lemma Ej315_1 : (forall x : U, A x -> B x) -> (forall x : U, A x) -> forall x : U, B x.
+Proof.
+  intros.
+  exact (H x (H0 x)).
+Qed.
+
+Lemma Ej315_2 : forall x : U, A x -> ~ (forall x : U, ~ A x).
+Proof.
+  unfold not.
+  intros.
+  exact (H0 x H).
+Qed.
+
+Lemma Ej315_3 : (forall x : U, P -> A x) -> P -> forall x : U, A x.
+Proof.
+  exact (fun (H1 : forall x : U, P -> A x) (H2 : P) (H3 : U) => H1 H3 H2).
+Qed.
+
+Lemma Ej315_4 : (forall x y : U, R x y) -> forall x : U, R x x.
+Proof.
+  exact (fun (H1 : forall x y : U, R x y) (H2 : U) => H1 H2 H2).
+Qed.
+
+Lemma Ej315_5 : (forall x y: U, R x y -> R y x) -> forall z : U, R e z -> R z e.
+Proof.
+  exact (fun (H1 : forall x y: U, R x y -> R y x) (z : U) => H1 e z).
+Qed.
+
+End Ejercicio15.
+
--- Practica3/plantilla3.v
+++ Practica3/plantilla3.v
@@ -139,16 +139,23 @@ Section Ejercicio5.
 (* 5.1 *)
 Definition opI (A : Set) (x : A) := x.
 
-Definition opK ...
+Definition opK (A: Set) (x: A) (y: A) := x.
 
-Definition opS ...
+Definition opS (A: Set) (f: A->A->A) (g: A->A) (x: A) := (f x) (g x).
 
-(* 5.2 *)
+(*
+opI = def [x] x
+opK = def [x][y] x
+opS = def [f][g][x] ((f x) (g x))
+*)
+
+(* 5.2 
 (* Para formalizar el siguiente lema, determine los tipos ?1 ... ?8 adecuados *)
 Lemma e52 : forall A B : Set, opS ?1 ?2 ?3 (opK ?4 ?5) (opK ?6 ?7) = opI ?8.
 Proof.
 
 Qed.
+*)
 
 End Ejercicio5.
 
@@ -159,49 +166,50 @@ Definition Zero (X : Set) (o : X) (f : X -> X) := o.
 Definition Uno  (X : Set) (o : X) (f : X -> X) := f (Zero X o f).
 
 (* 6.1 *)
-Definition Dos  ...
+Definition Dos (X: Set) (o: X) (f: X -> X) := f (Uno X o f).
 
 (* 6.2 *)
-Definition Succ ...
+Definition Succ (n: N) (X: Set) (o: X) (f: X -> X) := f (n X o f).
 
 Lemma succUno : Succ Uno = Dos.
 Proof.
-
+compute.
+reflexivity.
 Qed.
 
 (* 6.3 *)
-Definition Plus (n m : N) : N
-                := fun (X : Set) (o : X) (f : X -> X) => n X (m X o f) f.
+Definition Plus (n m : N) : N := fun (X : Set) (o : X) (f : X -> X) => n X (m X o f) f.
 
+Infix "_++_" := Plus (left associativity, at level 94).
 
-Infix "++" := Plus (left associativity, at level 94).
-
-Lemma suma1: (Uno ++ Zero) = Uno.
+Lemma suma1: (Uno _++_ Zero) = Uno.
 Proof.
-
+compute.
+reflexivity.
 Qed.
 
-Lemma suma2: (Uno ++ Uno) = Dos.
+Lemma suma2: (Uno _++_ Uno) = Dos.
 Proof.
-
+compute.
+reflexivity.
 Qed.
 
 (* 6.4 *)
-Definition Prod (n m : N) : N
-                := fun (X:Set) (o:X) (f:X->X) => m X o (fun y:X => n X y f).
+Definition Prod (n m : N) : N := fun (X:Set) (o:X) (f:X->X) => m X o (fun y:X => n X y f).
 
-
-Infix "**" := ...
+Infix "**" := Prod (left associativity, at level 94).
 
 (* 6.5 *)
 Lemma prod1 : (Uno ** Zero) = Zero.
 Proof.
-
+compute.
+reflexivity.
 Qed.
 
 Lemma prod2: (Uno ** Dos) = Dos.
 Proof.
-
+compute.
+reflexivity.
 Qed.
 
 End Ejercicio6.
@@ -209,32 +217,38 @@ End Ejercicio6.
 
 Section Ejercicio7.
 (* 7.1 *)
-Definition Bool := ...
-Definition t    := ...
-Definition f    := ...
+Definition Bool := forall X: Set, X -> X -> X.
+Definition t (X: Set) (t0: X) (f0: X) := t0.
+Definition f (X: Set) (t0: X) (f0: X) := f0.
+
+Check (t: Bool).
+Check (f: Bool).
 
 (* 7.2 *)
-Definition If ...
+Definition If (b: Bool) (tb: Bool) (fb: Bool) (X: Set) (x y: X) := b X (tb X x y) (fb X x y).
 
 (* 7.3 *)
-Definition Not ...
+Definition Not (b: Bool) (X: Set) (x y: X) := b X y x.
 
-Lemma CorrecNot : (Not t) = f /\ (Not f) = t.
+Lemma CorrectNot : (Not t) = f /\ (Not f) = t.
 Proof.
-
+split; compute; reflexivity.
 Qed.
 
 (* 7.4 *)
-Definition And ...
+Definition And (a: Bool) (b: Bool) (X: Set) (x y: X) := a X (b X x y) (f X x y).
 
-Definition And' ...
+Definition And' (a: Bool) (b: Bool) (X: Set) (x y: X) := b X (a X x y) (f X x y).
 
 (* 7.5 *)
-Infix "&" := ...
+Infix "&" := And (left associativity, at level 94).
 
 Lemma CorrecAnd : (t & t) = t /\ (f & t) = f /\ (t & f) = f.
 Proof.
-
+split.
+compute.
+reflexivity.
+split;compute;reflexivity.
 Qed.
 
 End Ejercicio7.
@@ -249,31 +263,37 @@ Parameter empty    : ArrayNat 0.
 Parameter add      : forall n:nat, nat -> ArrayNat n -> ArrayNat (n + 1).
 
 (* 8.1 *)
+Check ((add 0 (S 0) empty) : ArrayNat (S 0)).
 
 (* 8.2 *)
+(* Vector [0,0] *)
+Check (add 1 0 (add 0 0 empty): ArrayNat 2).
+
+(* Vector [0,1,0,1 *)
+Check (add 3 0 (add 2 1 (add 1 0 (add 0 1 empty))): ArrayNat 4).
 
 (* 8.3 *)
-Parameter Concat : ...
+Parameter Concat : forall n1: nat, forall n2 :nat, ArrayNat n1 -> ArrayNat n2 -> ArrayNat (plus n1 n2). 
 
 (* 8.4 *)
-Parameter Zip : ...
+Parameter Zip : forall n: nat, ArrayNat n -> ArrayNat n -> (nat->nat->nat) -> ArrayNat n.
 
 (* 8.5 *)
+Check (ArrayNat: nat -> Set).
 
 (* 8.6 *)
-Parameter ArrayGen : ...
-Parameter emptyGen : ...
-Parameter addGen   : ...
-Parameter ZipGen   : ...
+Parameter ArrayGen : Set -> nat -> Set.
+Parameter emptyGen : forall X: Set, ArrayGen X 0.
+Parameter addGen   : forall X: Set, forall n: nat, X -> ArrayGen X n -> ArrayGen X (n+1).
+Parameter ZipGen   : forall X: Set, forall n: nat, ArrayGen X n -> ArrayGen X n -> (nat->nat->nat) -> ArrayGen X n.
 
 (* 8.7 *)
-Parameter ArrayBool : ...
+Parameter ArrayBool : forall n: nat, ArrayGen bool n.
 
 End ArrayNat.
 
 
 Section Ejercicio9.
-...
 End Ejercicio9.
 
 
@@ -284,18 +304,22 @@ Parameter emptyA : forall X : Set, Array X 0.
 Parameter addA : forall (X : Set) (n : nat), X -> Array X n -> Array X (S n).
 
 Parameter Matrix : Set -> nat -> Set.
-Parameter emptyM : ...
-Parameter addM : ...
+Parameter emptyM : forall (X : Set) (n : nat), Matrix (Array X n) 0.
+Parameter addM : forall (X : Set) (m n : nat), Array X m -> Matrix (Array X m) n -> Matrix (Array X m) (S n).
 
-Definition M1 := ... (* matriz de una columna *)
-Definition M2 := ... (* matriz de dos columnas *) 
-Definition M3 := ... (* matriz de tres columnas *)
+Definition A1 := addA nat 2 1 (addA nat 1 0 (addA nat 0 0 (emptyA nat))). (*columna de longitud 3 con un 1 al inicio*)
+Definition A2 := addA nat 2 2 (addA nat 1 2 (addA nat 0 0 (emptyA nat))).
+Definition A3 := addA nat 2 3 (addA nat 1 3 (addA nat 0 3 (emptyA nat))).
+
+Definition M1 := addM nat 3 0 A1 (emptyM nat 3).  (* matriz de una columna *)
+Definition M2 := addM nat 3 1 A2 M1. (* matriz de dos columnas *) 
+Definition M3 := addM nat 3 2 A3 M2. (* matriz de tres columnas *)
 
 Check M3.
 
 End Ejercicio10.
 
-
+(*
 Section Ejercicio11.
 ...
 End Ejercicio11.
@@ -304,24 +328,24 @@ End Ejercicio11.
 Section Ejercicio12.
 ...
 End Ejercicio12.
-
+*)
 
 Section Ejercicio13.
 Variable A B C: Set.
 
 Lemma e13_1 : (A -> B -> C) -> B -> A -> C.
 Proof.
-  ...
+exact (fun (f: A -> B -> C) (b: B) (a: A) => f a b).
 Qed.
 
 Lemma e13_2 : (A -> B) -> (B -> C) -> A -> C.
 Proof.
-  ...
+exact (fun (f: A -> B) (g: B -> C) (a: A) => g (f a)).
 Qed.
 
 Lemma e13_3 : (A -> B -> C) -> (B -> A) -> B -> C.
 Proof.
-  ...
+exact (fun (f: A -> B -> C) (g: B -> A) (b: B) => f (g b) b).
 Qed.
 
 End Ejercicio13.
@@ -333,19 +357,19 @@ Variable A B C: Prop.
 Lemma Ej314_1 : (A -> B -> C) -> A -> (A -> C) -> B -> C.
 Proof.
   intros f a g b.
-        ...
+  exact (g a).
 Qed.
 
 Lemma Ej314_2 : A -> ~ ~ A.
 Proof.
   unfold not.
   intros.
-     ...
+  exact (H0 H).
 Qed.
 
 Lemma Ej314_3 : (A -> B -> C) -> A -> B -> C.
 Proof.
-     ...
+  exact (fun (f: A -> B -> C) => f).
 Qed.
 
 Lemma Ej314_4 : (A -> B) -> ~ (A /\ ~ B).
@@ -353,7 +377,7 @@ Proof.
   unfold not.
   intros.
   elim H0; intros.
-     ...
+  exact (H2 (H H1)).
 Qed.
 
 End Ejercicio14.
@@ -368,34 +392,32 @@ Variable A B : U -> Prop.
 Variable P : Prop.
 Variable R : U -> U -> Prop.
 
-Lemma Ej315_1 : (forall x : U, A x -> B x) -> (forall x : U, A x) ->
-forall x : U, B x.
+Lemma Ej315_1 : (forall x : U, A x -> B x) -> (forall x : U, A x) -> forall x : U, B x.
 Proof.
   intros.
-   ...
+  exact (H x (H0 x)).
 Qed.
 
 Lemma Ej315_2 : forall x : U, A x -> ~ (forall x : U, ~ A x).
 Proof.
   unfold not.
   intros.
-  ...
+  exact (H0 x H).
 Qed.
 
 Lemma Ej315_3 : (forall x : U, P -> A x) -> P -> forall x : U, A x.
 Proof.
-    ...
+  exact (fun (H: (forall x : U, P -> A x)) (p: P) (x: U) => H x p).
 Qed.
 
 Lemma Ej315_4 : (forall x y : U, R x y) -> forall x : U, R x x.
 Proof.
-     ...
+  exact (fun (H: (forall x y : U, R x y)) (x: U) => H x x).
 Qed.
 
-Lemma Ej315_5 : (forall x y: U, R x y -> R y x) ->
-                 forall z : U, R e z -> R z e.
+Lemma Ej315_5 : (forall x y: U, R x y -> R y x) -> forall z : U, R e z -> R z e.
 Proof.
-     ...
+  exact (fun (H: (forall x y: U, R x y -> R y x)) (z: U) => H e z).
 Qed.
 
 End Ejercicio15.
