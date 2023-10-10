9f20be2c2a9c6fab244dbf635d102dd0e2473c9b
Katherine Sullivan <64032284+katherinesullivan@users.noreply.github.com>
2023-10-10 10:53:31 -0300

    Update p4

--- practica4k.v
+++ practica4k.v
@@ -71,6 +71,12 @@ Definition And :=
                      | _, _ => false
   end.
 
+Definition Not := 
+  fun b:bool => match b with
+                  true => false
+                 | false => true
+  end.
+
 Definition Xor := 
   fun b1 b2: bool => match b1, b2 with
                       true, false => true
@@ -87,6 +93,83 @@ Definition is_nil (A:Set) :=
   end.
 
 End Ej2.
+
+Section Ej3.
+
+Fixpoint add (n m:nat) {struct n}: nat :=
+  match n with
+      0 => m
+    | S k => S (add k m)
+  end.
+
+Fixpoint prod (n m:nat) {struct n}: nat :=
+  match n with
+    0 => 0
+   | S 0 => m
+   | S x => add m (prod x m)
+  end.
+
+Fixpoint pot (n m:nat) {struct m}: nat :=
+  match m with
+    0 => S 0
+   | S k => prod n (pot n k)
+  end.
+
+Definition isZero :=
+  fun n:nat => match n with
+                  0 => true
+                 | S x => false
+  end.
+
+Fixpoint leBool (n m:nat) {struct n}: bool :=
+  match n, m with
+    0, 0 => false
+   | 0, S y => true
+   | S x, 0 => false
+   | S x, S y => leBool x y
+  end. 
+
+(*prod pot leBool*)
+
+
+End Ej3.
+
+Section Ej4.
+
+Fixpoint length (A:Set) (l : list A) {struct l} : nat :=
+  match l with
+    nil _ => 0
+   | cons _ x l1 => S (length A l1)
+  end.
+
+Check length.
+
+Fixpoint append (A:Set) (l1 l2: list A) {struct l1} : list A :=
+  match l1 with
+    nil _ => l2
+   | cons _ x tl1 => cons A x (append _ tl1 l2) 
+  end.
+
+Fixpoint reverse (A:Set) (l:list A) {struct l} : list A :=
+  match l with
+    nil _ => nil A
+   | cons _ x tl => append A (reverse A tl) (cons A x (nil A))
+  end.
+
+  
+(*
+1. length: (forall A: Set, list A -> nat), que calcula la longitud de una lista.
+2. append: (forall A: Set, list A -> list A -> list A), que concatena dos listas.
+3. reverse: (forall A: Set, list A -> list A), que invierte una lista.
+4. filter: (forall A: Set, (A -> bool) -> list A -> list A), que deja en una
+lista los elementos que cumplen cierta propiedad.
+5. map: (forall A B: Set, (A -> B) -> list A -> list B), que aplica una función
+a todos los elementos de una lista.
+6. exists_: (forall A: Set, (A -> bool) -> list A -> bool), que devuelve true
+si y sólo si hay por lo menos un elemento en la lista que cumple cierta propiedad.
+*)
+
+End Ej4.
 
 Section Ej8.
 
@@ -118,17 +201,73 @@ Qed.
 
 Lemma LAnd : forall a b : bool, And a b = true <-> a = true /\ b = true.
 Proof.
+intros. 
+unfold iff.
+split.
+- destruct a; simpl; destruct b; intros; split;
+  try repeat reflexivity || apply H.
+- intros. destruct a; destruct b; simpl.
+  * reflexivity.
+  * elim H. intros. apply H1.
+  * elim H. intros. apply H0.
+  * elim H. intros. apply H1.
+Qed.
+
+Lemma LOr1 : forall a b : bool, Or a b = false <-> a = false /\ b = false.
+Proof.
 intros.
 unfold iff.
 split.
-- destruct a.
-  * simpl. destruct b. 
-      intros. split; reflexivity.
-      intros. split. reflexivity. apply H.
- * simpl. destruct b.
-      intros 
+- destruct a; destruct b; auto.
+    (*;simpl * intros. split. 
+      apply H. 
+      destruct b. apply H. reflexivity.
+    * destruct b; intros; split.
+      reflexivity. 
+      apply H. 
+      reflexivity. 
+      reflexivity.  *)
+- intros. destruct a; destruct b; simpl; elim H; auto. (*intros; try apply H0 || apply H1.*)
+Qed.
 
+Lemma LOr2 : forall a b : bool, Or a b = true <-> a = true \/ b = true.
+Proof.
+intros. unfold iff. split.
+- destruct a; destruct b; auto.
+- intros. destruct a.
+  * simpl. reflexivity.
+  * destruct b; simpl. 
+      reflexivity.
+      elim H; intros; apply H0.
+Qed.
+
+Lemma LXor : forall a b : bool, Xor a b = true <-> a <> b.
+Proof.
+intros. unfold iff. split.
+- destruct a; destruct b; simpl; intros.
+  absurd (false=true). discriminate. apply H.
+  discriminate.
+  discriminate.
+  discriminate.
+- intros.
+  destruct a; destruct b; simpl; auto.
+  * absurd (true <> true).
+    unfold not. intros. apply H0. reflexivity.
+    apply H.
+  * absurd (false <> false).
+    unfold not. intros. apply H0. reflexivity.
+    apply H.
+Qed.
+
+Lemma LNot : forall b : bool, Not (Not b) = b.
+Proof.
+intros. destruct b; simpl; reflexivity.
+Qed.
+ 
 End Ej8.
+
+Section Ej9.
+End Ej9. 
  
 
 
